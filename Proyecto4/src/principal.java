5ae272f5de86fcc76402585aa51592b2a7defd20
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-09-24 20:30:24 +0200

    0.12

--- Proyecto4/src/principal.java
+++ Proyecto4/src/principal.java
@@ -2,9 +2,7 @@
 public class principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-		System.out.println("Cambio para slack");
-		System.out.println("Cambio para slack");
+		System.out.println("Luego hacemos otro repo");
 
 	}
 
